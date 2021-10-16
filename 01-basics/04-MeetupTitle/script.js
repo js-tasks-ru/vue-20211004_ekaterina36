33a9f42b1f1100850a357cde6945c09a69b7a7e6
Kate Tsareva <katetsareva3012@mail.ru>
2021-10-17 00:07:14 +0300

    chore: add vue app MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,19 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+const app = createApp({
+  data() {
+    return {
+      meetupTitle: null,
+      meetupNum: null,
+    };
+  },
+
+  watch: {
+    meetupNum() {
+      fetchMeetupById(this.meetupNum).then((meetup) => {
+        this.meetupTitle = meetup.title;
+      });
+    }
+  },
+}).mount('#app');
