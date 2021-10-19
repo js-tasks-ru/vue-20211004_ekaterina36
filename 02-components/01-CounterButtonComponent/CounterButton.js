b9924cb42a4dbcbf9d85459c2c0908c77a78b51f
Kate Tsareva <katetsareva3012@mail.ru>
2021-10-19 13:32:56 +0300

    chore: add functional counterBtn

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,23 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    btnClick() {
+      this.$emit('update:count', this.count + 1);
+    },
+  },
 
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button" @click="btnClick">{{ count }}</button>`,
 });
